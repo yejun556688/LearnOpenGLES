c02b496bedbdc0bf401d8502ad995514f33fdc16
林伟池 <loying@foxmail.com>
2016-03-25 11:40:12 +0800

    立体贴图

--- Tutorial01-GLKit/LearnOpenGLES/ViewController.m
+++ Tutorial01-GLKit/LearnOpenGLES/ViewController.m
@@ -10,51 +10,50 @@
 
 @interface ViewController ()
 
-@property (nonatomic , strong) EAGLContext* context;
-@property (nonatomic , strong) GLKBaseEffect* effect;
+@property (nonatomic , strong) EAGLContext* mContext;
+@property (nonatomic , strong) GLKBaseEffect* mEffect;
 
-@property (nonatomic , assign) GLuint myProgram;
-
+@property (nonatomic , assign) int mCount;
 @end
 
 @implementation ViewController
-
+{
+    dispatch_source_t timer;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
    
 
     //新建OpenGLES 上下文
-    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-    
+    self.mContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     GLKView* view = (GLKView *)self.view;
-    view.context = self.context;
+    view.context = self.mContext;
     view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
-    
-    [EAGLContext setCurrentContext:self.context];
+    [EAGLContext setCurrentContext:self.mContext];
     glEnable(GL_DEPTH_TEST);
     
     
-    
+    //顶点数据
     GLfloat squareVertexData[48] =
     {
         0.5, -0.5, 0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f, //右下
-        -0.5, 0.5, -0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f, //左上
-        -0.5, -0.5, -0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f, //左下
+        -0.5, 0.5, 0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f, //左上
+        -0.5, -0.5, 0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 0.0f, //左下
         
         0.5, 0.5, -0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 1.0f, //右上
-//        -0.5f, 0.5f, -0.0f,    0.0f, 0.0f, 1.0f,   0.0f, 1.0f, //左上
-//        0.5f, -0.5f, -0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f //右下
     };
-    
-    
-    GLubyte indices[] =
+
+    //顶点索引
+    GLuint indices[] =
     {
         0, 1, 2,
         1, 3, 0
     };
+    self.mCount = sizeof(indices) / sizeof(GLuint);
 
     
+    //顶点数据缓存
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -65,33 +64,26 @@
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * 8, (char *)NULL + 0);
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 4 * 8, (char *)NULL + 0);
 
     glEnableVertexAttribArray(GLKVertexAttribNormal);
     glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 4 * 8, (char *)NULL + 12);
 
     glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
     glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 4 * 8, (char *)NULL + 24);
+
     
-    glDrawArrays(GL_TRIANGLES, 0, 6);
-    
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    
+    //纹理贴图
     NSString* filePath = [[NSBundle mainBundle] pathForResource:@"for_test" ofType:@"png"];
-    
     NSDictionary* options = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderOriginBottomLeft, nil];
     GLKTextureInfo* textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
-    
-    self.effect = [[GLKBaseEffect alloc] init];
-    self.effect.light0.enabled = GL_TRUE;
-    self.effect.light0.diffuseColor = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
-    
-    self.effect.texture2d0.enabled = GL_TRUE;
-    self.effect.texture2d0.name = textureInfo.name;
+    //着色器
+    self.mEffect = [[GLKBaseEffect alloc] init];
+    self.mEffect.light0.enabled = GL_TRUE;
+    self.mEffect.light0.diffuseColor = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
+    self.mEffect.texture2d0.enabled = GL_TRUE;
+    self.mEffect.texture2d0.name = textureInfo.name;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -99,38 +91,34 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 /**
  *  场景数据变化
  */
 - (void)update {
-    CGSize size = self.view.bounds.size;
-    float aspect = fabsf(size.width / size.height);
-    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0), 1.0f, 0.1f, 10.f);
-    projectionMatrix = GLKMatrix4Scale(projectionMatrix, 1.0f, 1.0f, 1.0f);
-    self.effect.transform.projectionMatrix = projectionMatrix;
     
-    GLKMatrix4 modelViewMatrix = GLKMatrix4Translate(GLKMatrix4Identity, 0.0f, 0.0f, -1.0f);
-    self.effect.transform.modelviewMatrix = modelViewMatrix;
+    //投影变换
+    CGSize size = self.view.bounds.size;
+    float aspect = fabs(size.width / size.height);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(90.0), aspect, 0.1f, 10.f);
+    projectionMatrix = GLKMatrix4Scale(projectionMatrix, 1.0f, 1.0f, 1.0f);
+    self.mEffect.transform.projectionMatrix = projectionMatrix;
+    
+    //z轴平移
+    GLKMatrix4 modelViewMatrix = GLKMatrix4Translate(GLKMatrix4Identity, 0.0f, 0.0f, -2.0f);
+    self.mEffect.transform.modelviewMatrix = modelViewMatrix;
 }
 
 
 /**
  *  渲染场景代码
- *
- *  @param view <#view description#>
- *  @param rect <#rect description#>
  */
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
     glClearColor(0.3f, 0.6f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    [self.effect prepareToDraw];
-    glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_BYTE, 0);
-//    glDrawArrays(GL_TRIANGLES, 0, 6);
-    
-    
-
+    //启动着色器
+    [self.mEffect prepareToDraw];
+    glDrawElements(GL_TRIANGLES, self.mCount, GL_UNSIGNED_INT, 0);
 }
 
 @end
