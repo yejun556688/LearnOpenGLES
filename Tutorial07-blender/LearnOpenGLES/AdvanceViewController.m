4fde97ffff6c6c2396e6ef02c8a1191f61c514e4
林伟池 <loying@foxmail.com>
2016-04-20 11:02:11 +0800

    blender

---
+++ Tutorial07-blender/LearnOpenGLES/AdvanceViewController.m
@@ -0,0 +1,134 @@
+//
+//  AdvanceViewController.m
+//  LearnOpenGLES
+//
+//  Created by 林伟池 on 16/3/25.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "AdvanceViewController.h"
+#import "cube.h"
+
+
+@interface AdvanceViewController ()
+{
+    float   _rotate;
+}
+
+@property (nonatomic , strong) EAGLContext* mContext;
+
+@property (strong, nonatomic) GLKBaseEffect *baseEffect;
+
+@end
+
+
+@implementation AdvanceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    //新建OpenGLES 上下文
+    self.mContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    GLKView* view = (GLKView *)self.view;
+    view.context = self.mContext;
+    view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    
+    [EAGLContext setCurrentContext:self.mContext];
+    
+    self.baseEffect = [[GLKBaseEffect alloc] init];
+    
+    self.baseEffect.light0.enabled = GL_TRUE;
+    self.baseEffect.light0.position = GLKVector4Make(0.0f, 1.0f, 0.0f, 1.0f);
+    self.baseEffect.light0.specularColor = GLKVector4Make(0.25f, 0.25f, 0.25f, 1.0f);
+    self.baseEffect.light0.diffuseColor = GLKVector4Make(0.75f, 0.75f, 0.75f, 1.0f);
+    self.baseEffect.lightingType = GLKLightingTypePerPixel;
+    
+    [self setMatrices];
+    
+    // 1
+    // Positions
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 0, cubePositions);
+    
+    // Normals
+    glEnableVertexAttribArray(GLKVertexAttribNormal);
+    glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 0, cubeNormals);
+    
+    // Texels
+//    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+//    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 0, cubeTexels);
+    
+    
+    
+    NSDictionary* options = @{ GLKTextureLoaderOriginBottomLeft: @YES };
+    NSError* error;
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"cube.png" ofType:nil];
+    GLKTextureInfo* texture = [GLKTextureLoader textureWithContentsOfFile:path options:options error:&error];
+    
+    if(texture == nil)
+        NSLog(@"Error loading file: %@", [error localizedDescription]);
+    
+    self.baseEffect.texture2d0.name = texture.name;
+    self.baseEffect.texture2d0.enabled = true;
+    
+    
+    _rotate = 0.0f;
+    glEnable(GL_CULL_FACE);    
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setMatrices
+{
+    // Projection Matrix
+    const GLfloat aspectRatio = (GLfloat)(self.view.bounds.size.width) / (GLfloat)(self.view.bounds.size.height);
+    const GLfloat fieldView = GLKMathDegreesToRadians(90.0f);
+    const GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(fieldView, aspectRatio, 0.1f, 10.0f);
+    self.baseEffect.transform.projectionMatrix = projectionMatrix;
+    
+    // ModelView Matrix
+    GLKMatrix4 modelViewMatrix = GLKMatrix4Identity;
+    modelViewMatrix = GLKMatrix4Translate(modelViewMatrix, 0.0f, 0.0f, -5.0f);
+    modelViewMatrix = GLKMatrix4RotateX(modelViewMatrix, GLKMathDegreesToRadians(45.0f));
+    
+    modelViewMatrix = GLKMatrix4RotateY(modelViewMatrix, GLKMathDegreesToRadians(_rotate));
+    modelViewMatrix = GLKMatrix4RotateZ(modelViewMatrix, GLKMathDegreesToRadians(_rotate));
+    
+    self.baseEffect.transform.modelviewMatrix = modelViewMatrix;
+}
+
+- (void)update {
+    _rotate += 1.0f;
+}
+
+/**
+ *  渲染场景代码
+ */
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    [self setMatrices];
+    
+    // Render by parts
+    for(int i=0; i<cubeMaterials; i++)
+    {
+        // Set material
+        self.baseEffect.material.diffuseColor = GLKVector4Make(cubeDiffuses[i][0], cubeDiffuses[i][1], cubeDiffuses[i][2], 1.0f);
+        self.baseEffect.material.specularColor = GLKVector4Make(cubeSpeculars[i][0], cubeSpeculars[i][1], cubeSpeculars[i][2], 1.0f);
+        
+        // Prepare effect
+        [self.baseEffect prepareToDraw];
+        
+        // Draw vertices
+        glDrawArrays(GL_TRIANGLES, cubeFirsts[i], cubeCounts[i]);
+    }
+    
+}
+@end
+
