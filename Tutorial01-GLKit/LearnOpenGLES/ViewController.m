41f15e20874d6f28b546f514656eaef0b946697c
林伟池 <loying@foxmail.com>
2017-02-27 16:02:10 +0800

    代码优化

--- Tutorial01-GLKit/LearnOpenGLES/ViewController.m
+++ Tutorial01-GLKit/LearnOpenGLES/ViewController.m
@@ -9,11 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @property (nonatomic , strong) EAGLContext* mContext;
 @property (nonatomic , strong) GLKBaseEffect* mEffect;
-
-@property (nonatomic , assign) int mCount;
 @end
 
 @implementation ViewController
@@ -22,34 +19,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
+    [self setupConfig];
+    [self uploadVertexArray];
+    [self uploadTexture];
+}
 
+- (void)setupConfig {
     //新建OpenGLES 上下文
     self.mContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2]; //2.0，还有1.0和3.0
     GLKView* view = (GLKView *)self.view; //storyboard记得添加
     view.context = self.mContext;
     view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;  //颜色缓冲区格式
     [EAGLContext setCurrentContext:self.mContext];
-    
-   
-    
+}
+
+- (void)uploadVertexArray {
     //顶点数据，前三个是顶点坐标，后面两个是纹理坐标
     GLfloat squareVertexData[] =
     {
         0.5, -0.5, 0.0f,    1.0f, 0.0f, //右下
+        0.5, 0.5, -0.0f,    1.0f, 1.0f, //右上
+        -0.5, 0.5, 0.0f,    0.0f, 1.0f, //左上
+        
+        0.5, -0.5, 0.0f,    1.0f, 0.0f, //右下
         -0.5, 0.5, 0.0f,    0.0f, 1.0f, //左上
         -0.5, -0.5, 0.0f,   0.0f, 0.0f, //左下
-        0.5, 0.5, -0.0f,    1.0f, 1.0f, //右上
     };
-
-    //顶点索引
-    GLuint indices[] =
-    {
-        0, 1, 2,
-        1, 3, 0
-    };
-    self.mCount = sizeof(indices) / sizeof(GLuint);
-
     
     //顶点数据缓存
     GLuint buffer;
@@ -57,17 +52,13 @@
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertexData), squareVertexData, GL_STATIC_DRAW);
     
-    GLuint index;
-    glGenBuffers(1, &index);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
     glEnableVertexAttribArray(GLKVertexAttribPosition); //顶点数据缓存
     glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLfloat *)NULL + 0);
     glEnableVertexAttribArray(GLKVertexAttribTexCoord0); //纹理
     glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLfloat *)NULL + 3);
+}
 
-    
+- (void)uploadTexture {
     //纹理贴图
     NSString* filePath = [[NSBundle mainBundle] pathForResource:@"for_test" ofType:@"jpg"];
     NSDictionary* options = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderOriginBottomLeft, nil];//GLKTextureLoaderOriginBottomLeft 纹理坐标系是相反的
@@ -76,18 +67,6 @@
     self.mEffect = [[GLKBaseEffect alloc] init];
     self.mEffect.texture2d0.enabled = GL_TRUE;
     self.mEffect.texture2d0.name = textureInfo.name;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/**
- *  场景数据变化
- */
-- (void)update {
-    
 }
 
 
@@ -100,7 +79,13 @@
     
     //启动着色器
     [self.mEffect prepareToDraw];
-    glDrawElements(GL_TRIANGLES, self.mCount, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
